dafbfe8127d660ba221093fb1ab19ca3d46bab44
coolihans <97646033+coolihans@users.noreply.github.com>
2022-10-24 16:07:14 +0900

    feat: nickname refresh

--- frontend/src/components/CreateBean/CreateBean.tsx
+++ frontend/src/components/CreateBean/CreateBean.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import './CreateBean.scss'
 import x from '../../assets/img/x.svg'
 import 새로고침 from '../../assets/img/새로고침.svg'
+import { useState } from 'react'
 
 type createBeanProps = {
   CloseCreateBean: () => void;
 }
 
+
 function CreateBean({CloseCreateBean}: createBeanProps) {
+  let first = ["괜찮은", "평범한", "납작한", "멍청한", "똑똑한", "앙상한", "지적인", "관대한", "악독한", "방탕한"];
+  let second = ["치와와", "빈지노", "판다곰", "김우창", "강낭콩", "외국인", "도마뱀", "잠자리", " 하운드", "순례자"];
+  let nickname = first[Math.floor(Math.random()*first.length)] + ' ' +
+  second[Math.floor(Math.random()*second.length)]
+  let newNickName = first[Math.floor(Math.random()*first.length)] + ' ' +
+  second[Math.floor(Math.random()*second.length)]
+  const [refreshNickname, setRefreshNickname] = useState(nickname);
+  const RefreshNickname = () => {
+    setRefreshNickname(newNickName)
+  }
   return (
     <div className='create-bean'>
       <div className='header'>
@@ -17,8 +29,8 @@ function CreateBean({CloseCreateBean}: createBeanProps) {
       <div className='content'>
         <div className='name-style'>
           <div className='name-refresh'>
-            <p>괜찮은 치와와님,</p>
-            <img className='refresh' src={새로고침} alt="" />
+            <p>{refreshNickname}님,</p>
+            <img className='refresh' src={새로고침} alt="" onClick={RefreshNickname} />
           </div>
           <div className='style-button'>
             <p>스타일 변경</p>
