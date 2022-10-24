0065104932fc44e942179e5c039d1460682b9a05
qoqls1212 <qoqls1212@naver.com>
2022-10-24 15:58:54 +0900

    feat: 글 작성 모달

--- frontend/src/components/CreateBean/CreateBean.tsx
+++ frontend/src/components/CreateBean/CreateBean.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './CreateBean.scss'
 import x from '../../assets/img/x.svg'
+import Camera from '../../assets/img/Camera.svg'
+import Img_box from '../../assets/img/Img_box.svg'
 import 새로고침 from '../../assets/img/새로고침.svg'
 
 type createBeanProps = {
@@ -9,19 +11,43 @@ type createBeanProps = {
 
 function CreateBean({CloseCreateBean}: createBeanProps) {
   return (
-    <div className='create-bean'>
-      <div className='header'>
-        <h2>글 작성</h2>
-      </div>
-      <img className='x' src={x} alt="" onClick={CloseCreateBean}/>
-      <div className='content'>
-        <div className='name-style'>
-          <div className='name-refresh'>
-            <p>괜찮은 치와와님,</p>
-            <img className='refresh' src={새로고침} alt="" />
+    <div className='create-bean-back'>
+      <div className='create-bean'>
+        <div className='header'>
+          <h2>글 작성</h2>
+        </div>
+        <img className='x' src={x} alt="" onClick={CloseCreateBean}/>
+        <div className='content'>
+          <div className='name-style'>
+            <div className='name-refresh'>
+              <p>괜찮은 치와와님,</p>
+              <img className='refresh' src={새로고침} alt="" />
+            </div>
+            <div className='style-button'>
+              <p>스타일 변경</p>
+            </div>
           </div>
-          <div className='style-button'>
-            <p>스타일 변경</p>
+          <div className='message'>
+            <textarea placeholder='전하고 싶은 메시지를 입력해주세요.' />
+          </div>
+          <div className='camera-picture'>
+            <div className='camera'>
+              <img className='camera-img' src={Camera} alt="" />
+              <div>
+                <h4>카메라(선택)</h4>
+                <p>사진 첨부 시 사진 촬영</p>
+              </div>
+            </div>
+            <div className='picture'>
+              <img className='picture-img' src={Img_box} alt="" />
+              <div>
+                <h4>사진(선택)</h4>
+                <p>사진 첨부 시 이미지 첨부</p>
+              </div>
+            </div>
+          </div>
+          <div className='finish-button'>
+            <h3>글 작성 완료</h3>
           </div>
         </div>
       </div>
