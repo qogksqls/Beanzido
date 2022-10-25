a4bcd633bbe754fef08d42d561464086081d3d4c
qoqls1212 <qoqls1212@naver.com>
2022-10-25 20:49:33 +0900

    feat: 실시간 색상 변경

--- frontend/src/components/CreateBean/CreateBean.tsx
+++ frontend/src/components/CreateBean/CreateBean.tsx
@@ -1,122 +1,165 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './CreateBean.scss';
 import x from '../../assets/img/x.svg';
 import Camera from '../../assets/img/Camera.svg';
 import Img_box from '../../assets/img/Img_box.svg';
 import 새로고침 from '../../assets/img/새로고침.svg';
-import colorPicker from '../../assets/img/colorPicker.svg';
 
 type createBeanProps = {
   CloseCreateBean: () => void;
 };
 
 function CreateBean({ CloseCreateBean }: createBeanProps) {
-  let first = ["괜찮은", "평범한", "납작한", "멍청한", "똑똑한", "앙상한", "지적인", "관대한", "악독한", "방탕한"];
-  let second = ["치와와", "빈지노", "판다곰", "김우창", "강낭콩", "외국인", "도마뱀", "잠자리", " 하운드", "순례자"];
-  let nickname = first[Math.floor(Math.random()*first.length)] + ' ' +
-  second[Math.floor(Math.random()*second.length)]
-  let newNickName = first[Math.floor(Math.random()*first.length)] + ' ' +
-  second[Math.floor(Math.random()*second.length)]
+  let first = [
+    '괜찮은',
+    '평범한',
+    '납작한',
+    '멍청한',
+    '똑똑한',
+    '앙상한',
+    '지적인',
+    '관대한',
+    '악독한',
+    '방탕한',
+  ];
+  let second = [
+    '치와와',
+    '빈지노',
+    '판다곰',
+    '김우창',
+    '강낭콩',
+    '외국인',
+    '도마뱀',
+    '잠자리',
+    ' 하운드',
+    '순례자',
+  ];
+  let nickname =
+    first[Math.floor(Math.random() * first.length)] +
+    ' ' +
+    second[Math.floor(Math.random() * second.length)];
+  let newNickName =
+    first[Math.floor(Math.random() * first.length)] +
+    ' ' +
+    second[Math.floor(Math.random() * second.length)];
   const [refreshNickname, setRefreshNickname] = useState(nickname);
   const RefreshNickname = () => {
-    setRefreshNickname(newNickName)
-  }
+    setRefreshNickname(newNickName);
+  };
 
-  const [changeStyle, setChangeStyle] = useState(false)
+  const [changeStyle, setChangeStyle] = useState(false);
   function OpenChangeStyle() {
-    setChangeStyle(true)
+    setChangeStyle(true);
   }
   function CloseChangeStyle() {
-    setChangeStyle(false)
+    setChangeStyle(false);
   }
+
   function BeanStyle() {
-    return <>
-    <div className='bean-style-back' onClick={CloseChangeStyle}></div>
-    <div className='bean-style'>
-      <div style={{display: 'flex', height: '100%', justifyContent: 'space-around'}}>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column', 
-          justifyContent: 'space-between'}}>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#BDDB79'}}></div>
-            <p>완두콩</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#F25F9B'}}></div>
-            <p>강낭콩</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#A6A6A6'}}></div>
-            <p>쥐눈이콩</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#F57329'}}></div>
-            <p>랜탈콩</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#FFE9A0'}}></div>
-            <p>병아리콩</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
+    const beanStyles1 = [
+      { name: '완두콩', color: '#BDDB79', textColor: 'black' },
+      { name: '강낭콩', color: '#F25F9B', textColor: 'black' },
+      { name: '쥐눈이콩', color: '#A6A6A6', textColor: 'black' },
+      { name: '랜탈콩', color: '#F57329', textColor: 'black' },
+      { name: '병아리콩', color: '#FFE9A0', textColor: 'black' },
+    ];
+    const beanStyles2 = [
+      { name: '녹두', color: '#377E19', textColor: 'white' },
+      { name: '땅콩', color: '#E6BD46', textColor: 'black' },
+      { name: '검은콩', color: '#4E4E4E', textColor: 'white' },
+      { name: '팥', color: '#CC3737', textColor: 'white' },
+      { name: '젤리빈', color: '#375E97', textColor: 'white' },
+    ];
+    function ChangeBeanStyle(e: any, props: {color: string, textColor: string}) {
+      e.preventDefault();
+      if (props.color === '#4E4E4E') {
+        document.documentElement.style.setProperty('--create-bean-color', props.color);
+        document.documentElement.style.setProperty('--create-text-color', props.textColor);
+      } else {
+        document.documentElement.style.setProperty('--create-bean-color', props.color);
+        document.documentElement.style.setProperty('--create-text-color', props.textColor);
+      }
+    }
+    return (
+      <>
+        <div className="bean-style-back" onClick={CloseChangeStyle}></div>
+        <div className="bean-style">
+          <div
+            style={{
+              display: 'flex',
+              height: '100%',
+              justifyContent: 'space-around',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
+              {beanStyles1.map((beanStyle, idx) => {
+                return (
+                  <>
+                    <div
+                      className="bean-color"
+                      key={idx}
+                      onClick={(e) => {
+                        ChangeBeanStyle(e, beanStyle);
+                      }}
+                    >
+                      <div
+                        className="circle"
+                        style={{ backgroundColor: `${beanStyle.color}` }}
+                      ></div>
+                      <p>{beanStyle.name}</p>
+                    </div>
+                    <div
+                      style={{ display: 'flex', flexDirection: 'row-reverse' }}
+                    >
+                      <hr style={{ margin: '0', width: '70%' }} />
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
+              {beanStyles2.map((beanStyle, idx) => {
+                return (
+                  <>
+                    <div
+                      className="bean-color"
+                      key={idx}
+                      onClick={(e) => {
+                        ChangeBeanStyle(e, beanStyle);
+                      }}
+                    >
+                      <div
+                        className="circle"
+                        style={{ backgroundColor: `${beanStyle.color}` }}
+                      ></div>
+                      <p>{beanStyle.name}</p>
+                    </div>
+                    <div
+                      style={{ display: 'flex', flexDirection: 'row-reverse' }}
+                    >
+                      <hr style={{ margin: '0', width: '70%' }} />
+                    </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column', 
-          justifyContent: 'space-between'}}>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#377E19'}}></div>
-            <p>녹두</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#E6BD46'}}></div>
-            <p>땅콩</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#4E4E4E'}}></div>
-            <p>검은콩</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#CC3737'}}></div>
-            <p>팥</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-          <div className='bean-color'>
-            <div className='circle' style={{ backgroundColor: '#375E97'}}></div>
-            <p>젤리빈</p>
-          </div>
-          <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
-            <hr style={{margin: '0', width: '70%'}}/>
-          </div>
-        </div>
-      </div>
-    </div>
-    </>
+      </>
+    );
   }
 
   return (
@@ -130,8 +173,14 @@ function CreateBean({ CloseCreateBean }: createBeanProps) {
         <div className="content">
           <div className="name-style">
             <div className="name-refresh">
-              <h4>{refreshNickname}</h4><p>님,</p>
-              <img className="refresh" src={새로고침} alt="" onClick={RefreshNickname} />
+              <h4>{refreshNickname}</h4>
+              <p>님,</p>
+              <img
+                className="refresh"
+                src={새로고침}
+                alt=""
+                onClick={RefreshNickname}
+              />
             </div>
             <div className="style-button" onClick={OpenChangeStyle}>
               <p>스타일 변경</p>
