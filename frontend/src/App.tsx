1cfc52fa482a3d5b6775d602e03d6fd6eb9499ae
taehee <tevem1207@naver.com>
2022-10-27 12:34:40 +0900

    refactor: 🐢 웹소켓 구조 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,71 @@
-import { useState } from "react";
-import { RecoilRoot } from "recoil";
+import { useState, useEffect, useCallback } from "react";
+import { useRecoilState } from "recoil";
+import { beanListState } from "store/atom";
 import "./App.scss";
 import KakaoMap from "components/KakaoMap";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
 import useGeoLocation from "components/hooks/useGeolocation";
 import CreateBean from "./components/CreateBean/CreateBean";
 import createButton from "./assets/img/chat-button.svg";
 import Sidebar from "components/Sidebar/Sidebar";
 
 function App() {
+  const [beanList, setBeanList] = useRecoilState(beanListState);
+
   const location = useGeoLocation();
   const [isCreateBean, setIsCreateBean] = useState(false);
   const [isSideBar, setisSideBar] = useState(true);
+  const socketurl = process.env.REACT_APP_SOCKET_URL
+    ? process.env.REACT_APP_SOCKET_URL
+    : "";
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketurl);
 
-  const BeanList = [
-    {
-      Position: location.coordinates,
-      nickname: "괜찮은 황태희",
-      contents:
-        "안녕하세요 이게 되는지 잘 모르겠네요 이거는 20줄까지는 하고 싶은데",
-      color: "red",
-      img: "",
-      createdAt: Date(),
-    },
-  ];
+  useEffect(() => {
+    if (lastMessage !== null) {
+      if (lastMessage.data[0] == "{") {
+        setBeanList([...beanList, JSON.parse(lastMessage.data)]);
+      }
+    }
+  }, [lastMessage]);
+
+  const handleClickSendMessage = useCallback(
+    (bean: string) => sendMessage(bean),
+    []
+  );
+
+  const dto = JSON.stringify({
+    latitude: 37.5009614732362,
+    longitude: 127.03972084911923,
+    nickname: "괜찮은 황태희",
+    content:
+      "안녕하세요 이게 되는지 잘 모르겠네요 이거는 20줄까지는 하고 싶은데",
+    color: "1",
+    img: "",
+    createdAt: Date(),
+  });
 
   return (
-    <RecoilRoot>
-      <div className="App">
-        <img
-          className="create-button"
-          onClick={() => setIsCreateBean(true)}
-          src={createButton}
-          alt="chat-button"
-        />
-        {isCreateBean && <CreateBean setIsCreateBean={setIsCreateBean} />}
-        <KakaoMap MyPosition={location.coordinates} />
-        <Sidebar
-          isSideBar={isSideBar}
-          setisSideBar={setisSideBar}
-          BeanList={BeanList}
-        />
+    <div className="App">
+      <img
+        className="create-button"
+        onClick={() => setIsCreateBean(true)}
+        src={createButton}
+        alt="chat-button"
+      />
+      <div style={{ position: "absolute", zIndex: 100 }}>
+        <button
+          onClick={() => handleClickSendMessage(dto)}
+          disabled={readyState !== ReadyState.OPEN}
+        >
+          Click Me to send 'Hello'
+        </button>
+        {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
       </div>
-    </RecoilRoot>
+      {isCreateBean && <CreateBean setIsCreateBean={setIsCreateBean} />}
+      <KakaoMap MyPosition={location.coordinates} />
+      <Sidebar isSideBar={isSideBar} setisSideBar={setisSideBar} />
+    </div>
   );
 }
 
