993150c8d92d54bb28a02b92b799d6e879d66f09
qoqls1212 <qoqls1212@naver.com>
2022-10-29 00:24:26 +0900

    refactor: 피드백 css

--- frontend/src/components/FeedbackButton/FeedbackButton.tsx
+++ frontend/src/components/FeedbackButton/FeedbackButton.tsx
@@ -1,22 +1,40 @@
-import React, { useState, Dispatch, SetStateAction} from "react";
+import React, { useState, Dispatch, SetStateAction } from "react";
 import "./FeedbackButton.scss";
-
 
 type FeedbackProps = {
   setIsFeedbackButton: Dispatch<SetStateAction<boolean>>;
-}; 
+};
 
-export default function FeedbackButton({setIsFeedbackButton}: FeedbackProps) {
-
+export default function FeedbackButton({ setIsFeedbackButton }: FeedbackProps) {
   return (
     <>
-    <div
+      <div
         className="feedback-button-back"
         onClick={() => setIsFeedbackButton(false)}
       ></div>
-      <div className="feedback-box"
-            onClick={() => window.open('https://forms.gle/wVx625841uUueh9Q9', '_blank', 'noopener,noreferrer')}>
+      {/* <div
+        className="feedback-box"
+        onClick={() =>
+          window.open(
+            "https://forms.gle/wVx625841uUueh9Q9",
+            "_blank",
+            "noopener,noreferrer"
+          )
+        }
+      >
         <h3>Feedback for Beanzido!</h3>
+      </div> */}
+      <div
+        className="feedback"
+        onClick={() =>
+          window.open(
+            "https://forms.gle/wVx625841uUueh9Q9",
+            "_blank",
+            "noopener,noreferrer"
+          )
+        }
+      >
+        <h4>Beanzido를 위해 피드백을 남겨주세요!</h4>
       </div>
       {/* <button
       className="feedback-button"
@@ -26,4 +44,3 @@ export default function FeedbackButton({setIsFeedbackButton}: FeedbackProps) {
     </>
   );
 }
-
