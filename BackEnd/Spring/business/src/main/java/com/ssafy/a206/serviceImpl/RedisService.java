13b4f9579bd3e931cfa33eb248a5ae7a9c9a6d68
SSAFY <app928@naver.com>
2022-10-28 13:00:00 +0900

    session redis deleted

--- BackEnd/Spring/business/src/main/java/com/ssafy/a206/serviceImpl/RedisService.java
+++ BackEnd/Spring/business/src/main/java/com/ssafy/a206/serviceImpl/RedisService.java
@@ -1,20 +1,17 @@
 package com.ssafy.a206.serviceImpl;
 
-import java.time.Duration;
-
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Websocket;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.WebSocketSession;
 
-import lombok.RequiredArgsConstructor;
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class RedisService {
 	private final RedisTemplate<String, String> redisTemplateChat;
-	private final RedisTemplate<String, WebSocketSession> redisTemplateSession;
+//	private final RedisTemplate<String, String> redisTemplateSession;
 
 	public void setChatValues(String key, String data) {
 		ValueOperations<String, String> values = redisTemplateChat.opsForValue();
@@ -35,22 +32,22 @@ public class RedisService {
 		redisTemplateChat.delete(key);
 	}
 	
-	public void setSessionValues(String key, WebSocketSession data) {
-		ValueOperations<String, WebSocketSession> values = redisTemplateSession.opsForValue();
-		values.set(key, data);
-	}
-
-	public void setSessionValues(String key, WebSocketSession data, Duration duration) {
-		ValueOperations<String, WebSocketSession> values = redisTemplateSession.opsForValue();
-		values.set(key, data, duration);
-	}
-
-	public Object getSessionValues(String key) {
-		ValueOperations<String, WebSocketSession> values = redisTemplateSession.opsForValue();
-		return values.get(key);
-	}
-
-	public void deleteSessionValues(String key) {
-		redisTemplateSession.delete(key);
-	}
+//	public void setSessionValues(String key, WebSocketSession data) {
+//		ValueOperations<String, WebSocketSession> values = redisTemplateSession.opsForValue();
+//		values.set(key, data);
+//	}
+//
+//	public void setSessionValues(String key, WebSocketSession data, Duration duration) {
+//		ValueOperations<String, WebSocketSession> values = redisTemplateSession.opsForValue();
+//		values.set(key, data, duration);
+//	}
+//
+//	public Object getSessionValues(String key) {
+//		ValueOperations<String, WebSocketSession> values = redisTemplateSession.opsForValue();
+//		return values.get(key);
+//	}
+//
+//	public void deleteSessionValues(String key) {
+//		redisTemplateSession.delete(key);
+//	}
 }
