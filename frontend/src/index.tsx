7a8d6b315d2dc78b60849fa551dd0831bd6e9b66
taehee <tevem1207@naver.com>
2022-11-01 21:10:47 +0900

    feat: 🐢 serviceworker & favicon

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,8 +4,7 @@ import { RecoilRoot } from "recoil";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import registerServiceWorker from "./serviceWorker";
-registerServiceWorker();
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -18,7 +17,9 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
