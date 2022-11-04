8158e55d5c8594d0c6429c9a5dea7a7d5afee347
taehee <tevem1207@naver.com>
2022-11-04 14:55:53 +0900

    fix: 🐢 fix transiton

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 import { useRecoilState } from "recoil";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Routes, Route } from "react-router-dom";
 import { beanListState } from "store/atom";
 import { Outlet } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./Main.scss";
 import openIcon from "assets/img/Expand_right_light.svg";
-import x from "assets/img/x.svg"
-
+import x from "assets/img/x.svg";
+import useWebSocket from "react-use-websocket";
+import CreateBean from "components/CreateBean/CreateBean";
+import FeedbackButton from "components/FeedbackButton/FeedbackButton";
+import Sidebar from "components/Sidebar/Sidebar";
 import createButton from "assets/img/chat-button.svg";
 import FeedbackButtonGif from "assets/img/FeedbackButton.gif";
 import FeedbackButtonImg from "assets/img/FeedbackButton.png";
@@ -14,14 +18,31 @@ import logo from "assets/img/Logo.svg";
 import chat from "assets/img/Chat.svg";
 import bigChat from "assets/img/Chat_alt.svg";
 
-
 function Main() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [beanList, setBeanList] = useRecoilState(beanListState);
   const [isFirst, setisFirst] = useState(true);
-
+  const socketurl = process.env.REACT_APP_SOCKET_URL
+    ? process.env.REACT_APP_SOCKET_URL
+    : "";
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketurl, {
+    shouldReconnect: (closeEvent) => {
+      return true;
+    },
+    reconnectAttempts: 10,
+    reconnectInterval: 3000,
+  });
+  useEffect(() => {
+    if (lastMessage !== null) {
+      if (lastMessage.data[0] == "{") {
+        setBeanList([...beanList, JSON.parse(lastMessage.data)]);
+      }
+    }
+  }, [lastMessage]);
   function switchChat(target: number) {
     if (window.location.pathname === "/") {
-      navigate("/sidebar")
+      navigate("/sidebar");
     }
     if (target === 1) {
       setisFirst(true);
@@ -56,7 +77,23 @@ function Main() {
 
   return (
     <>
-      <Outlet />
+      <TransitionGroup component={null}>
+        <CSSTransition
+          classNames="transition"
+          timeout={500}
+          key={location.key}
+          onEnter={() => console.log(location.pathname)}
+        >
+          <Routes location={location}>
+            <Route path="sidebar" element={<Sidebar />} />
+            <Route
+              path="create"
+              element={<CreateBean sendMessage={sendMessage} />}
+            />
+            <Route path="feedback" element={<FeedbackButton />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
       <div className="create-button">
         <img
           className="create-button-img"
@@ -87,30 +124,33 @@ function Main() {
             <img src={chat} alt="상세보기" />
           </div>
         </div>
-        <div className="feedback-button"
-            onClick={clickFeedback}>
+        <div className="feedback-button" onClick={clickFeedback}>
           <img
             className="feedback-button-img"
             src={FeedbackButtonImg}
             alt="feedback-button"
           />
-          <img
-            className="feedback-button-img"
-            src={FeedbackButtonGif}
-            alt=""
-          />
+          <img className="feedback-button-img" src={FeedbackButtonGif} alt="" />
           <div>클릭 시 피드백 페이지로 이동</div>
           <div
             className="feedback-close"
             onClick={() => {
-              document.getElementsByClassName("feedback-button-center")[0].className =
-                "feedback-button"
-                navigate("/")}}>
+              document.getElementsByClassName(
+                "feedback-button-center"
+              )[0].className = "feedback-button";
+              navigate("/");
+            }}
+          >
             <img src={x} alt="" />
           </div>
         </div>
       </div>
-      <div className="handle" onClick={() => {navigate("/sidebar")}}>
+      <div
+        className="handle"
+        onClick={() => {
+          navigate("/sidebar");
+        }}
+      >
         <img src={openIcon} alt="open" />
       </div>
     </>
