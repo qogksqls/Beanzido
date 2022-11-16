aafcbf8bae8e8f152c10004488870c0c20d6aac2
taehee <tevem1207@naver.com>
2022-11-16 15:45:01 +0900

    fix: 🐢 tap bar size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,10 +21,11 @@ function App() {
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty("--vh", `${vh}px`);
     }
+    setScreenSize();
 
-    // window.addEventListener("resize", setScreenSize);
+    window.addEventListener("resize", setScreenSize);
 
-    // return () => window.removeEventListener("resize", setScreenSize);
+    return () => window.removeEventListener("resize", setScreenSize);
   }, []);
 
   return (
